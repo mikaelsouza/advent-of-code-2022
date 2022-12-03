8ce3e0c78b8ba50e305aafdab9fea4126b466965
Mikael Souza <mikael.souza.cc@gmail.com>
2022-12-03 18:38:21 -0400

    updated part1 to use sets as part2

--- src/day-3/part1.py
+++ src/day-3/part1.py
@@ -9,9 +9,7 @@ with open("input.txt") as f:
     final_value = 0
     for line in f:
         half = len(line) // 2
-        first_half, second_half = line[:half], line[half:]
-        for char in second_half:
-            if char in first_half:
-                final_value += char_to_score(char)
-                break
+        first_half, second_half = set(line[:half]), set(line[half:])
+        char = first_half.intersection(second_half).pop()
+        final_value += char_to_score(char)
     print(final_value)
