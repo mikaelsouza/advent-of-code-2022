34f6b0f9875719b641313520bbdb1a43310118c6
Mikael Souza <mikael.souza.cc@gmail.com>
2022-12-03 18:39:44 -0400

    updated part2

--- src/day-3/part2.py
+++ src/day-3/part2.py
@@ -16,6 +16,6 @@ groups = zip(lines[::3], lines[1::3], lines[2::3])
 score = 0
 
 for g1, g2, g3 in groups:
-    l = set(g1).intersection(set(g2)).intersection(set(g3))
+    l = set.intersection(set(g1), set(g2), set(g3))
     score += char_to_score(l.pop())
 print(score)
